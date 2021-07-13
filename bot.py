62e70e3ae53806bf7d73852ce0776033f197ab5e
GichanPark <85354875+GichanPark@users.noreply.github.com>
2021-07-14 01:00:02 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -139,32 +139,10 @@ async def letter(ctx, num):
     now=datetime.datetime.today()
     dis_day=datetime.strptime("20230125", "%Y%m%d")
     remain_day=dis_day-now
+    
+@bot.command(){
+async def test(ctx):
+    await ctx.send("v1")
+    
 token=os.environ['bot_token']
 bot.run(token)
-
-@bot.command(){
- async def test(ctx):
-     await ctx.send("v1")
-#bot.run("ODYwMzE5NTMzNTIzNzk1OTY4.YN5hAA.c7a3cAU8IRq18lfwqdkLjL_dMOY")
-
-# import discord
-# from discord.ext import commands
-# import os
-
-# client = commands.Bot(command_prefix = '-')
-
-# @client.event
-# async def on_ready():
-
-#   # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
-#   await client.change_presence(status=discord.Status.online)
-
-#   await client.change_presence(activity=discord.Game(name="게임 하는중"))
-#   #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
-#   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
-#   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
-  
-#   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
-
-
-# client.run(os.environ['token'])
